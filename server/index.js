4ab2c0a16c349550b1aae8e541d8911fa02b6c0e
MilindShekhawat <milindshekhawat10@gmail.com>
2023-12-08 14:51:30 +0530

    feat: implemented a joinroom socket connection.
    Implemented a joinroom event. When user clicks on join button, name and roomId are sent to the server and the page redirects to room/[roomId].

--- server/index.js
+++ server/index.js
@@ -1,23 +1,37 @@
-import express from 'express'
-import { createServer } from 'node:http'
-import { Server } from 'socket.io'
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io'; //
 
-const app = express()
-const server = createServer(app)
+const app = express();
+const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: '*',
   },
-})
+});
 
+//TODO Temp maps to store username-socketid relationship. Replace with DB
+const userToSocketMap = new Map();
+const socketToUserMap = new Map();
+
+//Socket Logic
 io.on('connection', (socket) => {
-  console.log(`Socket Connected:`, socket.id)
-  socket.emit('me', socket.id)
+  console.log(`Socket Connected:`, socket.id);
 
   socket.on('disconnect', () => {
-    console.log(`Socket Disconnected:`, socket.id)
-    socket.broadcast.emit('callended')
-  })
-})
+    console.log(`Socket Disconnected:`, socket.id);
+  });
 
-server.listen(5000, () => console.log('Server running on port 5000'))
+  //JoinRoom Logic
+  socket.on('joinroom', (payload) => {
+    const { username, room } = payload;
+    userToSocketMap.set(username, socket.id);
+    socketToUserMap.set(socket.id, username);
+
+    io.to(room).emit('userjoined', { username, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit('joinroom', { username, room });
+  });
+});
+
+server.listen(5000, () => console.log(`Server running on port 5000`));
