94ed16da859148fd41ff9e574888dd6e7d9398b7
MilindShekhawat <milindshekhawat10@gmail.com>
2023-12-08 16:32:54 +0530

    feat: create peerService class and its logic.
    The peer service class is used to establish connections between two peers. Stun servers are given. Offers and answers are also created.

---
+++ client/src/services/peer.js
@@ -0,0 +1,40 @@
+class PeerService {
+  constructor() {
+    if (!this.peer) {
+      this.peer = new RTCPeerConnection({
+        iceServers: [
+          {
+            urls: [
+              'stun:stun.l.google.com:19302',
+              'stun:global.stun.twilio.com:3478',
+            ],
+          },
+        ],
+      });
+    }
+  }
+
+  getAnswer = async (offer) => {
+    if (this.peer) {
+      await this.peer.setRemoteDescription(offer);
+      const answer = await this.peer.createAnswer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(answer));
+      return answer;
+    }
+  };
+
+  getOffer = async () => {
+    if (this.peer) {
+      const offer = await this.peer.createOffer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(offer));
+      return offer;
+    }
+  };
+
+  setLocalDescription = async (answer) => {
+    if (this.peer) {
+      await this.peer.setRemoteDescription(new RTCSessionDescription(answer));
+    }
+  };
+}
+export default new PeerService();
