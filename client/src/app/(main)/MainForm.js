4387a50687c87553c4c0da7ac41f403b8611a817
MilindShekhawat <milindshekhawat10@gmail.com>
2023-12-08 14:51:30 +0530

    feat: implemented a joinroom socket connection.
    Implemented a joinroom event. When user clicks on join button, name and roomId are sent to the server and the page redirects to room/[roomId].

--- client/src/app/(main)/MainForm.js
+++ client/src/app/(main)/MainForm.js
@@ -1,5 +1,9 @@
 'use client';
 import { useState } from 'react';
+import Link from 'next/link';
+import socket from '@/lib/socket';
+import { useRouter } from 'next/navigation';
+
 import {
   Card,
   CardContent,
@@ -13,47 +17,66 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 
 export default function MainForm() {
+  const router = useRouter();
   const [username, SetUsername] = useState('');
   const [room, SetRoom] = useState('');
 
-  const handleSubmit = (e) => {};
+  //Emits joinroom event to server
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit('joinroom', { username, room });
+    handleJoinRoom();
+  };
+
+  //Listens for joinroom event from server and redirects to room page
+  const handleJoinRoom = () => {
+    socket.on('joinroom', (payload) => {
+      console.log('JoinRoom Payload:', payload);
+      router.push(`/room/${payload.room}`);
+    });
+    return () => {
+      socket.off('joinroom');
+    };
+  };
 
   return (
-    <main className='flex justify-center'>
-      <Card className='w-80'>
-        <CardHeader>
-          <CardTitle>Join Meeting</CardTitle>
-          <CardDescription>Enter your name and the meeting id.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={() => handleSubmit()}>
-            <div className='grid items-center w-full gap-4'>
-              <div className='flex flex-col space-y-1.5'>
-                <Label htmlFor='username'>User Name</Label>
-                <Input
-                  id='username'
-                  type='text'
-                  value={username}
-                  onChange={(e) => SetUsername(e.target.value)}
-                />
-              </div>
-              <div className='flex flex-col space-y-1.5'>
-                <Label htmlFor='room'>Room</Label>
-                <Input
-                  id='room'
-                  type='text'
-                  value={room}
-                  onChange={(e) => SetRoom(e.target.value)}
-                />
-              </div>
+    <Card className='w-80'>
+      <CardHeader>
+        <CardTitle>Join Meeting</CardTitle>
+        <CardDescription>Enter your name and the meeting id.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form id='loginForm' onSubmit={(e) => handleSubmit(e)}>
+          <div className='grid items-center w-full gap-4'>
+            <div className='flex flex-col space-y-1.5'>
+              <Label htmlFor='username'>User Name</Label>
+              <Input
+                id='username'
+                type='text'
+                value={username}
+                onChange={(e) => SetUsername(e.target.value)}
+              />
             </div>
-          </form>
-        </CardContent>
-        <CardFooter className='flex justify-between'>
-          <Button variant=''>Join</Button>
-          <Button variant='destructive'>Cancel</Button>
-        </CardFooter>
-      </Card>
-    </main>
+            <div className='flex flex-col space-y-1.5'>
+              <Label htmlFor='room'>Room</Label>
+              <Input
+                id='room'
+                type='text'
+                value={room}
+                onChange={(e) => SetRoom(e.target.value)}
+              />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className='flex justify-between'>
+        <Button asChild form='loginForm'>
+          <Link href='/meet'>Join</Link>
+        </Button>
+        <Button variant='destructive' form='loginForm'>
+          Cancel
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
